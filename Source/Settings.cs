27d3e2d9ae2cf5890335a1796bc3e0e707d0d2d3
krafs <massa@krafs.net>
2019-04-27 23:11:08 +0200

    Added option for receiving letter. Adjusted sound volume.

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -8,10 +8,12 @@ namespace LevelUp
         {
             base.ExposeData();
             Scribe_Values.Look(ref allowLevelUpTextMessage, "AllowLevelUpTextMessage", true, true);
+            Scribe_Values.Look(ref allowLevelUpLetter, "AllowLevelUpLetter", false, true);
             Scribe_Values.Look(ref allowLevelUpSoundEffect, "AllowLevelUpSoundEffect", true, true);
             Scribe_Values.Look(ref allowLevelUpAnimation, "AllowLevelUpAnimation", true, true);
 
             Scribe_Values.Look(ref allowLevelDownTextMessage, "AllowLevelDownTextMessage", false, true);
+            Scribe_Values.Look(ref allowLevelDownLetter, "AllowLevelDownLetter", false, true);
             Scribe_Values.Look(ref allowLevelDownSoundEffect, "AllowLevelDownSoundEffect", false, true);
             Scribe_Values.Look(ref allowLevelDownAnimation, "AllowLevelDownAnimation", false, true);
         }
@@ -19,12 +21,14 @@ namespace LevelUp
         // Level Up
 
         public static bool allowLevelUpTextMessage;
+        public static bool allowLevelUpLetter;
         public static bool allowLevelUpSoundEffect;
         public static bool allowLevelUpAnimation;
 
         // Level Down
 
         public static bool allowLevelDownTextMessage;
+        public static bool allowLevelDownLetter;
         public static bool allowLevelDownSoundEffect;
         public static bool allowLevelDownAnimation;
     }
