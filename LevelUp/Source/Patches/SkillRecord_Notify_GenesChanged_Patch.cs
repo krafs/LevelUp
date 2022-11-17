a4fe6885994b44205fe261b6bff4ed700c0193ff
krafs <gratiano_facioplegia@aleeas.com>
2022-11-17 21:38:46 +0100

    Merge Biotech project with main project

---
+++ LevelUp/Source/Patches/SkillRecord_Notify_GenesChanged_Patch.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace LevelUp.Patches;
+
+[StaticConstructorOnStartup]
+public static class SkillRecord_Notify_GenesChanged_Patch
+{
+    static SkillRecord_Notify_GenesChanged_Patch()
+    {
+        if (!ModLister.BiotechInstalled)
+        {
+            return;
+        }
+        Logger.Debug("SkillRecord_Notify_GenesChanged_Patch");
+        Harmony harmony = new("Krafs.LevelUp.Biotech");
+
+        MethodInfo original = SymbolExtensions.GetMethodInfo<SkillRecord>(x => x.Notify_GenesChanged());
+        HarmonyMethod prefix = new(typeof(SkillRecord_Notify_GenesChanged_Patch), nameof(SkillRecord_Notify_GenesChanged_Patch.Prefix));
+        HarmonyMethod postfix = new(typeof(SkillRecord_Notify_GenesChanged_Patch), nameof(SkillRecord_Notify_GenesChanged_Patch.Postfix));
+        harmony.Patch(original, prefix, postfix);
+    }
+
+    private static void Prefix(out int __state, SkillRecord __instance)
+    {
+        __state = __instance.Level;
+    }
+
+    private static void Postfix(int __state, Pawn ___pawn, SkillRecord __instance)
+    {
+        int previousLevel = __state;
+        int currentLevel = __instance.Level;
+
+        if (currentLevel == previousLevel)
+        {
+            return;
+        }
+        else if (currentLevel > previousLevel)
+        {
+            Notifier.OnLevelUp(__instance, ___pawn);
+        }
+        else
+        {
+            Notifier.OnLevelDown(__instance, ___pawn);
+        }
+    }
+}
