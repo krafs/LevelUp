d6d864f9eb2bab8ce7d8886c322c3e2a455dde94
krafs <massa.krafs@proton.me>
2024-03-18 17:16:51 +0100

    Fix: Blank screen when editing xenogene on Create Characters-screen

--- Source/Patcher.cs
+++ Source/Patcher.cs
@@ -38,6 +38,14 @@ internal static class Patcher
     [SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "Harmony naming convention")]
     private static void DirtyAptitudesPostfix(int __state, SkillRecord __instance, Pawn ___pawn)
     {
+        // DirtyAptitudes can be called on the Create Character-screen if Biotech is used,
+        // and either crashes or makes it impossible to move forward.
+        // This causes the mod to try and display notifications for a colonist when not yet in a playable program state.
+        if (Current.ProgramState == ProgramState.Entry)
+        {
+            return;
+        }
+
         int previousLevel = __state;
         int currentLevel = __instance.Level;
 
