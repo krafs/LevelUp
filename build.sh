dd9f9acc481061ea1df9df76abb5987ae80816b9
krafs <massa.krafs@proton.me>
2025-07-23 20:46:14 +0200

    Clean up csproj

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-dotnet build -c "13"
-dotnet build -c "14"
-dotnet build -c "15"
-dotnet build -c "16"
+dotnet build -c V1_3
+dotnet build -c V1_4
+dotnet build -c V1_5
+dotnet build -c V1_6
 dotnet msbuild -target:CopyModFilesToModOutput
